4082dd1587c679ddcfcd3385258c2eb2a0ba2338
Валера Хлуд <85898243+Hval1987@users.noreply.github.com>
2021-10-30 15:06:52 +0300

    Delete task3 directory

--- task3/main/dao/ApplianceDAO.java
+++
@@ -1,8 +0,0 @@
-package com.task3.main.dao;
-
-import com.task3.main.model.Applianced;
-import com.task3.main.model.criteria.Criteria;
-
-public interface ApplianceDAO {
-	Applianced find(Criteria criteria);
-}
